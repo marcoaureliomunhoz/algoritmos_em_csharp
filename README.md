7d40e7f2bfe886a4fc9aa7a6f9dc4d19660bce0b
marcoaureliomunhoz <marcoaureliomunhoz@gmail.com>
2017-12-28 13:05:24 -0200

    SelectionSort1

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Algoritmos em C#
 
 **Algoritmos de Ordenação**
-- **Bubble Sort**: é o algoritmo mais simples, porém o menos eficiente também. A ideia é executar passos de ordenação de elementos pares/vizinhos enquanto houver movimentação de elementos.
+- **Bubble Sort**: é o algoritmo mais simples, porém o menos eficiente também. A ideia é executar passos de ordenação de elementos vizinhos enquanto houver movimentação/permutação.
+- **Selection Sort**: A ideia é executar passos de ordenação de elementos enquanto houver movimentação/permutação. Para cada elemento de posição i até n-1 o vetor é percorrido à direita a partir de i+1 até encontrar um elemento (i+1) menor que o elemento (i). Ao encontrar realiza-se a movimentação e incrementa i.
---
+++ apps/SelectionSort1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort1
+{
+    class Program
+    {
+        static void ImprimirVetor(string sobre, int[] vetor)
+        {
+            string aux = "";
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                aux += (aux.Length > 0 ? " | " : "") + vetor[i].ToString();
+            }
+            Console.WriteLine(sobre + "[ " + aux + " ]");
+        }
+
+        static void SelectionSort(int[] vetor)
+        {
+            ImprimirVetor("0 : ", vetor);
+
+            int i = 0;
+            int j = 0;
+            int aux = 0;
+            int passo = 0;
+            int comparacoes = 0;
+            int subComparacoes = 0;
+            int totalComparacoes = 0;
+            int movimentacoes = 0;
+            int totalMovimentacoes = 0;
+
+            do
+            {
+                movimentacoes = 0;
+                comparacoes = 0;
+                passo++;
+                for (i = 0; i < vetor.Length - 1; i++)
+                {
+                    subComparacoes = 0;
+                    j = i + 1;
+                    if (j < vetor.Length) subComparacoes++;
+                    while (j < vetor.Length && vetor[j] > vetor[i])
+                    {                        
+                        j++;
+                        if (j < vetor.Length) subComparacoes++;
+                    }
+                    if (j < vetor.Length && vetor[j] < vetor[i])
+                    {
+                        aux = vetor[j];
+                        vetor[j] = vetor[i];
+                        vetor[i] = aux;
+                        movimentacoes++;
+                    }
+                    totalMovimentacoes += movimentacoes;
+                    comparacoes += subComparacoes;
+                    //Console.WriteLine($"    Sub comparações: {subComparacoes}");
+                }
+                totalComparacoes += comparacoes;
+                ImprimirVetor($"{passo} : ", vetor);
+                Console.WriteLine($"    Comparações: {comparacoes}");
+            } while (movimentacoes > 1);
+
+            Console.WriteLine("");
+            Console.WriteLine("-- Resumo --");
+            Console.WriteLine($"Nro de elementos  (n)  : {vetor.Length}");
+            Console.WriteLine($"Qtde de passos    (p)  : {passo}");
+            Console.WriteLine($"Comparações       (C)  : {totalComparacoes}");
+            Console.WriteLine($"Movimentações     (M)  : {totalMovimentacoes}");
+            Console.WriteLine("");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("** Vetor desordenado - caso médio **");
+            SelectionSort(new int[] { 4, 7, 2, 9, 1, 6, 5, 3, 8 });
+
+            Console.WriteLine("** Vetor já ordenado - melhor caso **");
+            SelectionSort(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+
+            Console.WriteLine("** Vetor ordenado decrescente - pior caso **");
+            SelectionSort(new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 });
+
+            Console.ReadKey();
+        }
+    }
+}
